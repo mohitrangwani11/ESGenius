35d5f5affb3c0e7e58a5985a57dc9ca74a534dc2
mohitrangwani11 <107314777+mohitrangwani11@users.noreply.github.com>
2023-06-01 11:12:18 +1000

    Update app.py
    quotes were missing

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
-OPENAI_API_KEY=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
-HUGGINGFACEHUB_API_TOKEN=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
+OPENAI_API_KEY="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"
+HUGGINGFACEHUB_API_TOKEN="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"
 
 def get_pdf_text(pdf_docs):
     text = ""
