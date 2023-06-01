9c949d710f9723e0a20c18ea8ab293af15ff145e
mohitrangwani11 <107314777+mohitrangwani11@users.noreply.github.com>
2023-06-01 11:11:16 +1000

    Update app.py
    added api references

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
+OPENAI_API_KEY=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
+HUGGINGFACEHUB_API_TOKEN=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
+
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
