d40e568bc573d273a72187b258ae508c2fd0e09b
mohitrangwani11 <mohit.rangwani@gmail.com>
2023-06-01 10:59:57 +1000

    first initial commit all

---
+++ htmlTemplates.py
@@ -0,0 +1,45 @@
+#https://www.nicepng.com/maxp/u2q8q8a9w7q8a9w7/
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://www.imagehost.at/images/2023/05/26/Brother-Dogs.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
